0ea31490aab80c1b137c5111385049471ee91ac8
Gilberto Vargas <ticox.vargas@gmail.com>
2016-10-18 00:28:10 -0400

    carrusel listo me dio dolor de cabeza conseguir uno bueno.

--- layout/layout_sosmedica/header.php
+++ layout/layout_sosmedica/header.php
@@ -17,7 +17,12 @@
     <link href="<?php echo BASE_URL; ?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo BASE_URL; ?>public/css/jquery-ui.css" rel="stylesheet" type="text/css" /> 
     <link href="<?php echo BASE_URL; ?>public/css/jquery-ui.structure.css" rel="stylesheet" type="text/css" /> 
-     <link href="<?php echo BASE_URL; ?>public/css/tinycarousel.css" rel="stylesheet" type="text/css" /> 
+    <link href="<?php echo BASE_URL; ?>public/css/tinycarousel.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo BASE_URL; ?>public/css/slides.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>public/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>public/css/owl.theme.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>public/css/slides.css">
+    <link href="<?php echo BASE_URL; ?>public/css/font-awesome.min.css" rel="stylesheet" type="text/css" /> 
     <link href="<?php echo BASE_URL; ?>public/css/jquery-ui.theme.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo BASE_URL; ?>layout/layout3/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Plugin CSS -->
--- site/views/principal/js/index.js
+++ site/views/principal/js/index.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
 
+ $("#owl-demo").owlCarousel({
 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
 			$(document).on("click","#opt_",function(){
 
 
