193b5f6be3c11ad5c42658dcf44b161effd147bd
angel charlot <legna18205@gmail.com>
2016-12-01 19:39:42 -0400

    equivocacion ahora si tiene el link mail

---
+++ site/controllers/mailController.php
@@ -0,0 +1,24 @@
+<?php
+
+class mailController extends Controller
+{
+    private $_login;
+    
+    public function __construct(){
+        parent::__construct();
+       
+     
+    }
+    
+    public function index()
+    {
+
+        header('Location: https://sosmedica.com:2096/');
+    }
+    
+   
+
+
+}
+
+?>
